36f4602f965abb7c85f174b7dfe4f15c3f28fc6b
PerreuM <marie.perreu@univ-tlse3.fr>
2016-11-08 12:47:41 +0100

    pile

---
+++ src/main/java/deqo/mper/SimpleStackImpl.java
@@ -0,0 +1,32 @@
+package deqo.mper;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * Created by MarieP on 08/11/2016.
+ */
+public class SimpleStackImpl {
+    Stack<Object> wrappedStack = new Stack<Object>();
+
+    public boolean isEmpty(){
+        return wrappedStack.isEmpty();
+    }
+
+    public int getSize(){
+        return wrappedStack.size();
+    }
+
+    public void push(Item item){
+        wrappedStack.push(item);
+    }
+
+
+    public Item peek() throws EmptyStackException{
+        return (Item) wrappedStack.peek();
+    }
+
+    public Item pop() throws EmptyStackException {
+        return (Item) wrappedStack.pop();
+    }
+}
